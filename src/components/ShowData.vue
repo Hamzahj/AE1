8c448ac75f36e41a2919661a450ceb577255f066
Hamzah Javed <2javeh51@solent.ac.uk>
2019-04-16 12:26:00 +0000

    added images and consumed api

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -1,16 +1,28 @@
 <template>
+    
+    
 <div class="row">
     
-     <div class="col-12">  <h1 class='text-center'>Places that Visit</h1>   </div>
+    
+    
+     <div class="col-12">  <h1 class='text-center'>Tourist hotspots for you and your family!</h1>   </div>
     <div v-for="result in results" class="col-4">
-      <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="" alt="Card image cap">
+      <div class="card" style="width: 30rem;">
+ <img :src="result.picture.url">" 
   <div class="card-body">
-    <h5 class="card-title">{{result.name}}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+   <div>
+ 
 </div>
+     <h5 class="card-title">{{result.name}}</h5>
+    <h5 class="card-title">{{result.location.name}}</h5>
+    <p class="card-text">{{result.summary}}</p>
+    <p class="card-text">{{result.location.address}}</p>
+         <p class="card-text">:src{{result.picture.url}}</p>
+        
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+ 
+  </div>  -->
+</div> 
     </div>
     <!-- ./col-6 -->
     
@@ -19,7 +31,7 @@
 </div>
 <!-- ./row -->
     
-    
+   
     
    
   </div>
@@ -44,7 +56,7 @@
       
 
       const inst = axios.create({ headers: { 'Authorization': 'Bearer ' + this.key } })
-      inst.get('https://api.autoura.com/api/stops/search?group_context=friends&stop_types=food').then(r => {
+      inst.get('https://api.autoura.com/api/stops/search?group_context=friends&stop_types=tour').then(r => {
         this.results = r.data.response;
       }).catch(e => {
         console.log(e);
@@ -55,6 +67,11 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+body {
+  background-color: lightblue;
+}
+
+
 </style>
 
 
